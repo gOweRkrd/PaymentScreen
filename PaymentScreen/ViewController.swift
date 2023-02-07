a9ac97298ee5f22f8de66ac8ee2c023082ed5b80
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-02-07 19:12:50 +0700

    Add files via upload

---
+++ PaymentScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaymentScreen
+//
+//  Created by Александр Косяков on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
